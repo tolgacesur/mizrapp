63197edcc38a8c4e113f1b3fa3c14a541b06562a
Tolga Cesur <ttolgacesur@gmail.com>
2018-10-13 14:27:53 +0300

    Implementation of pymongo

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from pymongo import MongoClient()
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+db = MongoClient().mizrapp
 
 @app.route('/', methods=['GET'])
 def index():
