c55c97bd21390c77f9eccb69f1f7af223aeffeac
mbilalbark <muh.bark@gmail.com>
2018-10-14 01:50:06 +0300

    Product Featured is added

---
+++ app/src/components/Products/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  View } from 'react-native';
+
+class Products extends React.Component {
+
+ render() {
+     return(
+         <View>
+
+         </View>
+     )
+ }
+}
+
+export default Products
